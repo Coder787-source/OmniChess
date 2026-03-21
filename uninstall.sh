c1a954739eefeb6d5d0e7e2f20b1dbdb163b883d
Coder787-source <keshavkantamani@gmail.com>
2026-03-21 09:20:45 -0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
+install_dir="$HOME/OmniChess"
 
-echo "======================================"
-echo "      OmniChess Uninstaller"
-echo "======================================"
-
-# Remove OmniChess directory
-echo "Removing OmniChess files..."
-rm -rf "$HOME/OmniChess"
-
-# Remove desktop shortcuts (both locations)
-echo "Removing desktop shortcut..."
-sudo rm -f /usr/share/applications/omnichess.desktop
-rm -f ~/.local/share/applications/omnichess.desktop
-
-# Remove launcher
-echo "Removing launcher..."
-sudo rm -f /usr/local/bin/omnichess
-
-# Clear menu cache
-echo "Clearing menu cache..."
-sudo update-desktop-database /usr/share/applications/
-update-desktop-database ~/.local/share/applications/
-xdg-desktop-menu forceupdate --novendor
-rm -rf ~/.cache/menus
-
-echo "======================================"
-echo "OmniChess uninstalled successfully!"
-echo "Please log out and back in to"
-echo "complete removal from menu."
-echo "======================================"
+rm -rf "$install_dir"
+rm -f /usr/local/bin/omnichess
+remove_desktop_entry "OmniChess"
